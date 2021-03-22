c9d7fedcf3a97d66fc6ad4f6f5327e1ecbf18e7c
Rami <ramiahmedportland@gmail.com>
2021-03-22 11:54:41 -0700

    created a function derictory with scripts .js, index.html

---
+++ script.js
@@ -0,0 +1,40 @@
+
+'use strict';
+
+
+// let yourName = prompt("what is your name?"); {
+//   console.log("Hello "+ yourName + "!!!")
+// }
+
+// let newNumber1 = (5);
+// let newNumber2 =(3);
+// let newNumber3 =(3);
+// let threeTimes = newNumber1 * newNumber2 * newNumber3 ;
+
+// console.log(threeTimes);
+
+// let number1 = (5);
+// let number2 = (20);
+// let devide = number1 / number2 ;
+// console.log(devide);
+
+// let remainder = number1 % number2;
+// console.log(remainder);
+
+// const age = prompt("How old are you?");
+// let name1 = prompt("What is your name?");
+// let food = prompt("What is your favorite food?");
+
+// alert("Hello my name is " + name1 + "I am " + age + " years old and my favorite food is " + food);
+
+
+function add(number1, number2){
+  return number1 + number2;
+
+}
+
+const number1 = parseInt(prompt(" Enter a number: "));
+const number2 = parseInt(prompt(" Enter another number: "));
+alert(add(number1, number2));
+
+
